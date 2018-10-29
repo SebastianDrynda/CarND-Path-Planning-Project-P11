1acd34dcf52b3cc690e880388b50459f4ddd786d
Sebastian D <sebastian@drynda.com>
2018-10-29 12:33:58 +0100

    Implementation Path Planner

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "json.hpp"
+#include "spline.h"
 
 using namespace std;
 
@@ -74,17 +75,14 @@ int NextWaypoint(double x, double y, double theta, const vector<double> &maps_x,
 
   double heading = atan2((map_y - y), (map_x - x));
 
-  double angle = fabs(theta - heading);
-  angle = min(2 * pi() - angle, angle);
+  double angle = abs(theta - heading);
 
   if (angle > pi() / 4) {
     closestWaypoint++;
-    if (closestWaypoint == maps_x.size()) {
-      closestWaypoint = 0;
-    }
   }
 
   return closestWaypoint;
+
 }
 
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
@@ -200,9 +198,19 @@ int main() {
     map_waypoints_dy.push_back(d_y);
   }
 
+  // Debug information if true
+  bool debug = false;
+
+  // Our car's lane. Starting at middle lane.
+  int lane = 1;
+
+  // Reference velocity in mph
+  double ref_vel = 0.0;
+
   h.onMessage(
-      [&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
-          uWS::OpCode opCode) {
+      [&debug, &ref_vel, &lane, &map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy]
+      (uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
+
         // "42" at the start of the message means there's a websocket message event.
         // The 4 signifies a websocket message
         // The 2 signifies a websocket event
@@ -238,12 +246,215 @@ int main() {
               // Sensor Fusion Data, a list of all other cars on the same side of the road.
               auto sensor_fusion = j[1]["sensor_fusion"];
 
-              json msgJson;
+              // Provided previous path point size
+              int prev_size = previous_path_x.size();
 
+              if (prev_size > 0) {
+                car_s = end_path_s;
+              }
+
+              // Step 1 Prediction
+              bool car_ahead = false;
+              bool car_left = false;
+              bool car_right = false;
+              const int MIN_DISTANCE = 30;
+
+              for ( int i = 0; i < sensor_fusion.size(); i++ ) {
+                float d = sensor_fusion[i][6];
+
+                int car_lane = -1;
+
+                // Check if the car is on the same lane
+                if ( d > 0 && d < 4 ) {
+                  car_lane = 0;
+                } else if ( d > 4 && d < 8 ) {
+                  car_lane = 1;
+                } else if ( d > 8 && d < 12 ) {
+                  car_lane = 2;
+                } else if (car_lane < 0) {
+                  continue;
+                }
+
+                if (debug) {
+                  cout << "[DEBUG] Prediction car_lane: " << car_lane << endl;
+                }
+
+                // Check car velocity
+                double vx = sensor_fusion[i][3];
+                double vy = sensor_fusion[i][4];
+                double check_speed = sqrt(vx*vx + vy*vy);
+
+                // Estimate car s position after executing previous trajectory
+                double check_car_s = sensor_fusion[i][5];
+                check_car_s += ((double)prev_size*0.02*check_speed);
+
+                if (debug) {
+                  cout << "[DEBUG] Prediction check_speed: " << check_speed << ", check_car_s: " << check_car_s << endl;
+                }
+
+                if ( car_lane == lane ) {
+                  // Car in our lane
+                  car_ahead |= check_car_s > car_s && check_car_s - car_s < MIN_DISTANCE;
+                } else if ( car_lane - lane == -1 ) {
+                  // Car is left
+                  car_left |= car_s - MIN_DISTANCE < check_car_s && car_s + MIN_DISTANCE > check_car_s;
+                } else if ( car_lane - lane == 1 ) {
+                  // Car is right
+                  car_right |= car_s - MIN_DISTANCE < check_car_s && car_s + MIN_DISTANCE > check_car_s;
+                }
+
+                if (debug) {
+                  cout << "[DEBUG] Prediction car_ahead: " << car_ahead << ", car_left: " << car_left << ", car_right: " << car_right << endl;
+                }
+              }
+
+              // Step 2 Behavior Control
+              double speed_diff = 0;
+              const double MAX_SPEED = 49.35;
+              const double MAX_ACC = 0.220;
+
+              if ( car_ahead ) {  // Car ahead
+                if ( !car_left && lane > 0 ) {
+                  // Change lane left, if there is no car left and there is a left lane
+                  lane--;
+
+                  if (debug) {
+                    cout << "[DEBUG] Behavior car ahead, change lane left" << endl;
+                  }
+
+                } else if ( !car_right && lane != 2 ) {
+                  // Change lane right, if there is no car right and there is a right lane
+                  lane++;
+
+                  if (debug) {
+                    cout << "[DEBUG] Behavior car ahead, change lane right" << endl;
+                  }
+                } else {
+                  speed_diff -= MAX_ACC;
+                }
+              } else {
+                if ( lane != 1 ) {  // Back to center lane, if we are not on
+                  if ( ( lane == 0 && !car_right ) || ( lane == 2 && !car_left ) ) {
+                    lane = 1;
+
+                    if (debug) {
+                      cout << "[DEBUG] Behavior car not ahead, change lane back to center" << endl;
+                    }
+                  }
+                }
+                if ( ref_vel < MAX_SPEED ) {
+                  speed_diff += MAX_ACC;
+
+                  if (debug) {
+                    cout << "[DEBUG] Behavior ref_vel < MAX_SPEED: " << ref_vel << endl;
+                  }
+                }
+              }
+
+              // Step 3 Trajectory Generation
+              vector<double> ptsx;
+              vector<double> ptsy;
+
+              double ref_x = car_x;
+              double ref_y = car_y;
+              double ref_yaw = deg2rad(car_yaw);
+
+              // No previous trajectory
+              if ( prev_size < 2 ) {
+                double prev_car_x = car_x - cos(car_yaw);
+                double prev_car_y = car_y - sin(car_yaw);
+
+                ptsx.push_back(prev_car_x);
+                ptsx.push_back(car_x);
+
+                ptsy.push_back(prev_car_y);
+                ptsy.push_back(car_y);
+              } else {
+                // Use the last two points.
+                ref_x = previous_path_x[prev_size - 1];
+                ref_y = previous_path_y[prev_size - 1];
+
+                double ref_x_prev = previous_path_x[prev_size - 2];
+                double ref_y_prev = previous_path_y[prev_size - 2];
+                ref_yaw = atan2(ref_y-ref_y_prev, ref_x-ref_x_prev);
+
+                ptsx.push_back(ref_x_prev);
+                ptsx.push_back(ref_x);
+
+                ptsy.push_back(ref_y_prev);
+                ptsy.push_back(ref_y);
+              }
+
+              // Setting up three target points at a far distance to initialize the spline calculation
+              vector<double> next_wp0 = getXY(car_s + 30, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+              vector<double> next_wp1 = getXY(car_s + 60, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+              vector<double> next_wp2 = getXY(car_s + 90, 2 + 4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+
+              ptsx.push_back(next_wp0[0]);
+              ptsx.push_back(next_wp1[0]);
+              ptsx.push_back(next_wp2[0]);
+
+              ptsy.push_back(next_wp0[1]);
+              ptsy.push_back(next_wp1[1]);
+              ptsy.push_back(next_wp2[1]);
+
+              // Transform coordinates to local car coordinates
+              for ( int i = 0; i < ptsx.size(); i++ ) {
+                double shift_x = ptsx[i] - ref_x;
+                double shift_y = ptsy[i] - ref_y;
+
+                ptsx[i] = shift_x * cos(0 - ref_yaw) - shift_y * sin(0 - ref_yaw);
+                ptsy[i] = shift_x * sin(0 - ref_yaw) + shift_y * cos(0 - ref_yaw);
+              }
+
+              // Generating trajectory spline
+              tk::spline s;
+              s.set_points(ptsx, ptsy);
+
+              // Output path points from previous path for continuity.
               vector<double> next_x_vals;
               vector<double> next_y_vals;
+              for ( int i = 0; i < prev_size; i++ ) {
+                next_x_vals.push_back(previous_path_x[i]);
+                next_y_vals.push_back(previous_path_y[i]);
+              }
 
-              // TODO: define a path made up of (x,y) points that the car will visit sequentially every .02 seconds
+              // Calculate distance y position on 30 m ahead
+              double target_x = 30.0;
+              double target_y = s(target_x);
+              double target_dist = sqrt(target_x*target_x + target_y*target_y);
+
+              double x_add_on = 0;
+
+              for( int i = 1; i < 50 - prev_size; i++ ) {
+                ref_vel += speed_diff;
+                if ( ref_vel > MAX_SPEED ) {
+                  ref_vel = MAX_SPEED;
+                } else if ( ref_vel < MAX_ACC ) {
+                  ref_vel = MAX_ACC;
+                }
+
+                double N = target_dist/(0.02*ref_vel/2.24);
+                double x_point = x_add_on + target_x/N;
+                double y_point = s(x_point);
+
+                x_add_on = x_point;
+
+                double x_ref = x_point;
+                double y_ref = y_point;
+
+                x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+                y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+
+                x_point += ref_x;
+                y_point += ref_y;
+
+                next_x_vals.push_back(x_point);
+                next_y_vals.push_back(y_point);
+              }
+
+              json msgJson;
+
               msgJson["next_x"] = next_x_vals;
               msgJson["next_y"] = next_y_vals;
 
